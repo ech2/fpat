44e396e09abac6df14df50a2f74d2c9cc909c307
Eugene Cherny <iam@oscii.ru>
2018-09-24 14:00:58 +0300

    [r] rename Atom types into a more concise form

--- src/atom.h
+++ src/atom.h
@@ -7,49 +7,60 @@
 #ifndef FORMAL_PATCH_ATOM_H
 #define FORMAL_PATCH_ATOM_H
 
-#include "llvm/ADT/MapVector.h"
-#include "llvm/Support/StringPool.h"
-#include "eigen3/Eigen/Dense"
+#include <algorithm>
+#include <array>
 #include <map>
-#include <variant>
 #include <optional>
+#include <utility>
+#include <variant>
+
+#include "eigen3/Eigen/Dense"
+#include "llvm/Support/StringPool.h"
+
 #include "result.h"
 
 namespace fpat {
 
-using Number = float;
-using String = llvm::PooledStringPtr;
-using Vector = std::shared_ptr<Eigen::Matrix<Number, Eigen::Dynamic, 1>>;
-using Matrix = std::shared_ptr<Eigen::Matrix<Number, Eigen::Dynamic, Eigen::Dynamic>>;
-using BPF    = std::shared_ptr<Eigen::Matrix<Number, Eigen::Dynamic, 2>>;
+////
+//// atom types
+////
+
+using Num = float;
+using Str = llvm::PooledStringPtr;
+using Vec = std::shared_ptr<Eigen::Matrix<Num, Eigen::Dynamic, 1>>;
+using Mat = std::shared_ptr<Eigen::Matrix<Num, Eigen::Dynamic, Eigen::Dynamic>>;
+using Bpf = std::shared_ptr<Eigen::Matrix<Num, Eigen::Dynamic, 2>>;
 
 // TODO unit test to check that every other atom type is included into this type signature
-//using Dict   = llvm::MapVector<String, std::variant<Number, String, Vector, Matrix, BPF>>;
-using Dict   = std::map<String, std::variant<Number, String, Vector, Matrix, BPF>>;
+using Dic = std::map<Str, std::variant<Num, Str, Vec, Mat, Bpf>>;
 
-using Atom = std::variant<Number, String, Vector, Matrix, BPF, Dict>;
-using AtomArray = std::vector<Atom>;
+using Atom = std::variant<Num, Str, Vec, Mat, Bpf, Dic>;
+using AtomVec = std::vector<Atom>;
+
+////
+//// atom type operations
+////
 
 template<typename T>
 constexpr bool is_atom_type =
     // TODO unit test that checks that all actual Atom types are included in this check
-    std::is_same_v<T, Number>
-        || std::is_same_v<T, String>
-        || std::is_same_v<T, Vector>
-        || std::is_same_v<T, Matrix>
-        || std::is_same_v<T, BPF>;
+    std::is_same_v<T, Num>
+        || std::is_same_v<T, Str>
+        || std::is_same_v<T, Vec>
+        || std::is_same_v<T, Mat>
+        || std::is_same_v<T, Bpf>;
 
 template<typename T, typename = std::enable_if<is_atom_type<T>>>
 bool is_a(const Atom &x) {
   return std::holds_alternative<T>(x);
 }
 
-constexpr auto is_number = is_a<Number>;
-constexpr auto is_string = is_a<String>;
-constexpr auto is_vector = is_a<Vector>;
-constexpr auto is_matrix = is_a<Matrix>;
-constexpr auto is_bpf = is_a<BPF>;
-constexpr auto is_dict = is_a<Dict>;
+constexpr auto is_number = is_a<Num>;
+constexpr auto is_string = is_a<Str>;
+constexpr auto is_vector = is_a<Vec>;
+constexpr auto is_matrix = is_a<Mat>;
+constexpr auto is_bpf = is_a<Bpf>;
+constexpr auto is_dict = is_a<Dic>;
 
 template<typename T>
 Result<T> as(const Atom x) {
@@ -60,13 +71,14 @@ Result<T> as(const Atom x) {
   }
 }
 
-constexpr auto as_number = as<Number>;
-constexpr auto as_string = as<String>;
-constexpr auto as_vector = as<Vector>;
-constexpr auto as_matrix = as<Matrix>;
-constexpr auto as_bpf = as<BPF>;
-constexpr auto as_dict = as<Dict>;
+constexpr auto as_number = as<Num>;
+constexpr auto as_string = as<Str>;
+constexpr auto as_vector = as<Vec>;
+constexpr auto as_matrix = as<Mat>;
+constexpr auto as_bpf = as<Bpf>;
+constexpr auto as_dict = as<Dic>;
 
-} // namespace fpat
+
+}; // namespace fpat
 
 #endif  // FORMAL_PATCH_ATOM_H
