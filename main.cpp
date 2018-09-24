d838766206c033266b4d2ce77a2a8984592a8d3f
Eugene Cherny <iam@oscii.ru>
2018-09-24 13:39:56 +0300

    [*] upd dummy main

--- main.cpp
+++ main.cpp
@@ -8,17 +8,21 @@ using namespace fpat;
 
 int main() {
   llvm::StringPool p;
-  AtomArray aa{Number(1), String(p.intern("lol")), Number(2)};
+//  AtomVector aa{Num{1}, Str{p.intern("lol")}, Num{2}};
+//
+//  for (auto &a : aa) {
+//    std::cout << is_number(a) << std::endl;
+//  }
+//
+//  auto num = Result<int>(101);
+//  auto f = [](int x) { return std::to_string(x) + " lol"; };
+//
+//  auto t = num.fmap<std::string>(f);
+//  std::cout << t.get_value_or(":(") << std::endl;
 
-  for (auto &a : aa) {
-    std::cout << is_number(a) << std::endl;
-  }
-
-  auto num = Result<int>(101);
-  auto f = [](int x) { return std::to_string(x) + " lol"; };
-
-  auto t = num.fmap<std::string>(f);
-  std::cout << t.get_value_or(":(") << std::endl;
+//  AtomVec avec{Num{1}, Str{p.intern("lol")}, Num{2}};
+//  std::cout << match<Num, Str>(avec) << std::endl;
+//  std::cout << match_exact<Num, Str>(avec) << std::endl;
 
   return 0;
 }
