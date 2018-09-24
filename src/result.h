9cdcc39c0d833210ac7d129a65877cdc6b0d2236
Eugene Cherny <iam@oscii.ru>
2018-09-24 13:50:02 +0300

    [r] Result: add const noexcept signatures

--- src/result.h
+++ src/result.h
@@ -20,23 +20,24 @@ template<typename T>
 class Result : private std::variant<T, Error> {
  public:
   Result() = delete;
-  explicit Result(T x) : std::variant<T, Error>(x) {};
-  explicit Result(Error x) : std::variant<T, Error>(x) {};
+  Result(T &x) : std::variant<T, Error>(x) {};
+  Result(const T x) : std::variant<T, Error>(x) {};
+  Result(Error x) : std::variant<T, Error>(x) {};
 
-  bool is_error() const {
+  bool is_error() const noexcept {
     return std::get_if<0>(this) == nullptr;
   }
 
-  T *get_value_n() {
+  T *get_value_n() const noexcept {
     return std::get_if<0>(this);
   }
 
-  Error *get_error_n() {
+  Error *get_error_n() const noexcept {
     assert(std::get_if<1>(this) != nullptr);
     return std::get_if<1>(this);
   }
 
-  std::optional<T> get_value() {
+  std::optional<T> get_value() const noexcept {
     if (auto v = get_value_n()) {
       return std::make_optional(*v);
     } else {
@@ -44,7 +45,7 @@ class Result : private std::variant<T, Error> {
     }
   }
 
-  T get_value_or(T other) {
+  T get_value_or(T other) const noexcept {
     if (auto v = get_value_n()) {
       return *v;
     } else {
@@ -53,7 +54,7 @@ class Result : private std::variant<T, Error> {
   }
 
   template<typename Other>
-  Result<Other> fmap(std::function<Other(const T)> f) {
+  Result<Other> fmap(std::function<Other(const T)> f) const noexcept {
     if (is_error()) {
       return Result<Other>(*get_error_n());
     } else {
@@ -62,7 +63,7 @@ class Result : private std::variant<T, Error> {
   }
 
   template<typename Other>
-  Result<Other> map(std::function<Result<Other>(const T)> f) {
+  Result<Other> map(std::function<Result<Other>(const T)> f) const noexcept {
     if (is_error()) {
       return Result<Other>(get_error_n().value());
     } else {
